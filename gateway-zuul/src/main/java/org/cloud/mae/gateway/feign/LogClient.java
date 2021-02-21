d812a3ffee74a256da205746700a936901590891
nanachi1027 <nanachi1027@163.com>
2021-02-22 03:16:44 +0800

    add gateway controller

--- gateway-zuul/src/main/java/org/cloud/mae/gateway/feign/LogClient.java
+++ gateway-zuul/src/main/java/org/cloud/mae/gateway/feign/LogClient.java
@@ -1,5 +1,6 @@
 package org.cloud.mae.gateway.feign;
 
+import org.cloud.mae.log.Log;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
  */
 @FeignClient("log-center")
 public interface LogClient {
-    // @PostMapping("/logs-anon/internal")
-    // void save(@RequestBody Log log);
+    @PostMapping("/logs-anon/internal")
+    void save(@RequestBody Log log);
 }
