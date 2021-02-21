d44d9088d7aa16a4b42fb969a9f0cae05029116d
nanachi1027 <nanachi1027@163.com>
2021-02-22 02:24:48 +0800

    add gateway module impl

---
+++ gateway-zuul/src/main/java/org/cloud/mae/gateway/feign/LogClient.java
@@ -0,0 +1,15 @@
+package org.cloud.mae.gateway.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @Author: Mae
+ * @Date: 2021/2/22 1:54 上午
+ */
+@FeignClient("log-center")
+public interface LogClient {
+    // @PostMapping("/logs-anon/internal")
+    // void save(@RequestBody Log log);
+}
